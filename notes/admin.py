0a6891783c949b0dba1baa72d37511148caa25ce
Mandar Hemraj Patil <86347341+Mandarhp77@users.noreply.github.com>
2023-06-16 15:45:52 +0530

    Delete notes directory

--- notes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Homework, Notes, Todo
-# Register your models here.
-
-admin.site.register(Notes)
-admin.site.register(Homework)
-admin.site.register(Todo)
